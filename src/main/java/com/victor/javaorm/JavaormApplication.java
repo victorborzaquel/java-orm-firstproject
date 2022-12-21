0ab26359517667e0f31e2073559f6e5411463957
Victor Borzaquel <victorborzaquel@outlook.com>
2022-12-21 11:07:23 -0300

    Initial commit

---
+++ src/main/java/com/victor/javaorm/JavaormApplication.java
@@ -0,0 +1,13 @@
+package com.victor.javaorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaormApplication.class, args);
+	}
+
+}
